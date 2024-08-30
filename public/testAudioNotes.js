fe02dd9fb3f18d2a2ed5499e8b182b9390451d58
Ayush Bhanot <ayushbhanot1010@gmail.com>
2024-08-30 01:52:27 -0400

    Implemented Front-End UI

--- public/testAudioNotes.js
+++ public/testAudioNotes.js
@@ -2,31 +2,7 @@ const { generateNotes } = require('../src/services/openAIService'); // Adjust th
 const { processAudioFile } = require('../src/services/audioToNotesService'); // Assuming this function handles transcription
 const { organizeNotesByTopic } = require('../src/services/noteOrganizer'); // The file where organizeNotesByTopic is defined
 const { transcribeAudio } = require('../src/services/transcriptionService');
-/*
-const runTest = async (audioFilePath) => {
-    console.log("Testing audio-to-notes conversion...");
-    
-    try {
-        console.log("Starting transcription...");
-        const transcript = await processAudioFile(audioFilePath);
-        console.log("Transcription successful:", transcript);
 
-        console.log("Generating notes from the transcript...");
-        const notes = await generateNotes(transcript);
-        if (notes) {
-            console.log("Notes generation successful:", notes);
-            
-            // Now we organize the notes by topic
-            console.log("Organizing notes by topic...");
-            const organizedNotes = organizeNotesByTopic(notes);
-            console.log("Organized Notes:", JSON.stringify(organizedNotes, null, 2));
-        } else {
-            console.error("Note generation failed.");
-        }
-    } catch (error) {
-        console.error("Error processing audio file:", error);
-    }
-};*/
 async function runTest(audioFilePath) {
     try {
         const transcript = await transcribeAudio(audioFilePath);
